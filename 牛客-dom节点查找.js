162d51bee7f5b8e66e4514073f1c2abe0a4d1050
sharry2578@126.com <email@example.com>
2019-05-03 22:00:28 +0800

    5/3 v3

---
+++ 牛客-dom节点查找.js
@@ -0,0 +1,9 @@
+function commonParentNode(oNode1, oNode2) {
+    while(oNode1){
+        if(oNode1.contains(oNode2)){
+            return oNode1
+        }else{
+            oNode1 = oNode1.parentNode
+        }
+    }
+}
