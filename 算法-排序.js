cf949995b9cd842d0337eaa51262d7dba5d61eb6
sharry <sharry2578@126.com>
2019-06-01 18:51:05 +0800

    6/1 V1

--- 算法-排序.js
+++ 算法-排序.js
@@ -1,27 +1,27 @@
 /* 斐那其列数列 */
-function fn(n){
-    if(n <= 0){
+function fn(n) {
+    if (n <= 0) {
         return 0;
-    }else if(n == 1){
+    } else if (n == 1) {
         return 1
-    }else{
-        return fn(n-1)+fn(n-2);
+    } else {
+        return fn(n - 1) + fn(n - 2);
     }
-   }
+}
 //console.log("aa:",fn(1));
-   
+
 
 /* 冒泡 */
-function bubbleSort(list){
+function bubbleSort(list) {
     var data = list.slice(0)
     var length = data.length
-    for(let i = 0 ;i<length;i++){
-        for(let j = 0 ; j<length-1-i;j++){
-            if(data[j]>data[j+1]){
+    for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - 1 - i; j++) {
+            if (data[j] > data[j + 1]) {
                 //交换
                 let buf = data[j]
-                data[j] = data[j+1]
-                data[j+1] = buf
+                data[j] = data[j + 1]
+                data[j + 1] = buf
             }
         }
     }
@@ -29,13 +29,13 @@ function bubbleSort(list){
 }
 
 /* selection */
-function selectionSort(list){
+function selectionSort(list) {
     var data = list.slice(0)
     var length = data.length
-    for(let i=0 ;i<length ;i++){
+    for (let i = 0; i < length; i++) {
         let min = i
-        for(let j=i ; j<length;j++){
-            if(data[min]>data[j]){
+        for (let j = i; j < length; j++) {
+            if (data[min] > data[j]) {
                 min = j
             }
         }
@@ -47,21 +47,21 @@ function selectionSort(list){
 }
 
 /* 插入排序 */
-function insertSort(list){
+function insertSort(list) {
     var data = list.slice(0)
     var length = data.length
-    for(let i = 0;i< length-1; i++){
-        var current = data[i+1]
-        let j =i
-        while(j>0){
-            if(data[j]>current){
-               data[j+1]=data[j]
-               j--
-            }else{
+    for (let i = 0; i < length - 1; i++) {
+        var current = data[i + 1]
+        let j = i
+        while (j > 0) {
+            if (data[j] > current) {
+                data[j + 1] = data[j]
+                j--
+            } else {
                 break;
             }
-        } 
-        data[j+1] = current
+        }
+        data[j + 1] = current
     }
     return data
 }
@@ -69,24 +69,24 @@ function insertSort(list){
 
 
 /* 快速排序 */
-function fastSort(list){
+function fastSort(list) {
     var data = list.slice(0)
-    if(data.length<=1)return data
-    var index = Math.floor(data.length/2)
-    var baseValue = data.splice(index,1)[0]
+    if (data.length <= 1) return data
+    var index = Math.floor(data.length / 2)
+    var baseValue = data.splice(index, 1)[0]
     var left = []
     var right = []
     data.forEach(element => {
-        if(element<baseValue) left.push(element)
+        if (element < baseValue) left.push(element)
         else right.push(element)
     });
-    return fastSort(left).concat(baseValue,fastSort(right))
+    return fastSort(left).concat(baseValue, fastSort(right))
 }
 
-var arr = [-2,14,6,2,3,1,5,-1,2.5]
-console.log("原数组：",arr)
-console.log("冒泡排序   O(n²) ：",bubbleSort(arr));
-console.log("选择排序   O(n²) ：",selectionSort(arr));
-console.log("插入排序   O(n²) : ",insertSort(arr));
-console.log("快速排序  O(nlogn) ",fastSort(arr));
+var arr = [-2, 14, 6, 2, 3, 1, 5, -1, 2.5]
+console.log("原数组：", arr)
+console.log("冒泡排序   O(n²) ：", bubbleSort(arr));
+console.log("选择排序   O(n²) ：", selectionSort(arr));
+console.log("插入排序   O(n²) : ", insertSort(arr));
+console.log("快速排序  O(nlogn) ", fastSort(arr));
 console.log(arr);
