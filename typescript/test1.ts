c1b8dda8d7af488bee4c53f7fb5d71bf4560faeb
sharry <sharry2578@126.com>
2019-06-12 19:46:55 +0800

    6/12 v2

--- typescript/test1.ts
+++ typescript/test1.ts
@@ -1,4 +1,23 @@
-var l:string = 'asd'
+var l: string = 'asd'
 var k = 23;
-var lad:string = 'asd';
+var lad: string = 'asd';
 console.log(l)
+
+function getInfo(name: string, age: string): string {
+    return `${name}---${age}---${name + age}`
+}
+console.log(getInfo('hh', '23'))
+
+
+//string
+//boolean
+//number
+//数组类型
+var arrr: Array<number> = [1, 2, 3];
+var arr: number[] = [1, 2];
+//元组类型
+var x: [string, number, boolean] = ['abc', 12, true];
+//enum
+enum Flag { success = 1, error = 2 };
+let s: Flag = Flag.success;
+let e: Flag = Flag.error;
